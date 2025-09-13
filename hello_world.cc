097b2191de80705038e9841d4ed68066e1777e16
Matt Hartnett <matthew.e.hartnett@gmail.com>
2025-09-12 21:23:05 -0600

    Ran through the weird and kind of pointless find and replace exercise with snips

--- hello_world.cc
+++ hello_world.cc
@@ -2,8 +2,21 @@
 
 
  
-////  SNIP 1 (Include necessary headers)
+#include <math.h>
+#include <unistd.h> // For sleep()
 
+// For Arduino, use:
+//#include <Chirale_TensorFlowLite.h>
+
+// could use int8 or float version, we'll use float
+//#include "hello_world_int8_model_data.h"
+#include "hello_world_float_model_data.h"
+
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_log.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
 
 using namespace std;
 
@@ -19,11 +32,28 @@ constexpr int kInferencesPerCycle = 200; // for ~6.6s on Arduino Nano 33 BLE Sen
 const float kXrange = 2.f * 3.14159265359f;
 
 
+// some globals used 
+const tflite::Model* model = nullptr;  // the model
+tflite::MicroInterpreter* interpreter = nullptr; // the interpreter (what can perform inference)
+TfLiteTensor* input = nullptr; // the input tensor (where we put the input data)
+TfLiteTensor* output = nullptr; // the output tensor (where we get the output data)
+float output_scale = 0.0f; // for quantized models, the scale used to convert the output data
+int output_zero_point = 0; // for quantized models, the zero point used to convert the output data
 
-//// SNIP 2  (define some globals used for the model and interpreter)
+// the tensor_arena is the memory used for tensors (inputs, outputs, and intermediate tensors)
+  // Determining the Arena size is a bit of trial/error. 
+  // Docs say The exact arena usage can be determined  using the RecordingMicroInterpreter.
+constexpr int kTensorArenaSize = 2000;
+alignas(16) uint8_t tensor_arena[kTensorArenaSize];  // Keep aligned to 16 bytes for CMSIS
 
 
-//// SNIP 3  (Define a custom operator resolver for the Hello World example.)
+// Define a custom operator resolver for the Hello World example.
+// This resolver only needs one operator, the FullyConnected operator 
+using HelloWorldOpResolver = tflite::MicroMutableOpResolver<1>;
+TfLiteStatus RegisterOps(HelloWorldOpResolver& op_resolver) {
+  TF_LITE_ENSURE_STATUS(op_resolver.AddFullyConnected());
+  return kTfLiteOk;
+}
 
 }  // namespace
 
@@ -42,15 +72,78 @@ void setup() {
      // 5. Get input and output tensors.
 
 
-  //// SNIP 4 (Initialize the target system.)
   
-  //// SNIP 5 (Map the model into a usable data structure.)
+  // 1. Initialize the target system.
 
-  //// SNIP 6 (Create an operator resolver.)
+  // The tflite::InitializeTarget function, invoked during startup, is actually a user-implemented function. 
+  // It serves as a call to the system_setup.cc file, which users are expected to implement as part of integrating 
+  // TensorFlow Lite for Microcontrollers. This function provides a location for the initialization of hardware 
+  // drivers, such as UART, SPI, etc.
+  // For Arduino, see:
+  // https://github.com/spaziochirale/Chirale_TensorFlowLite/blob/main/src/tensorflow/lite/micro/system_setup.cpp
 
-  //// SNIP 7 (Allocate tensors.)
+  tflite::InitializeTarget();
+  
+  // 2. Map the model into a usable data structure.
 
-  //// SNIP 8 (Get input and output tensors.)
+  // using float:
+  model = tflite::GetModel(g_hello_world_float_model_data);
+  // if using int8
+  // model = tflite::GetModel(g_hello_world_int8_model_data);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    MicroPrintf(
+        "Model provided is schema version %d not equal "
+        "to supported version %d.",
+        model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+
+
+  // 3. Create an operator resolver.
+
+  
+  static HelloWorldOpResolver op_resolver;  // static means the allocation of memory survives the function call
+  status = RegisterOps(op_resolver);
+  if (status != kTfLiteOk) {
+    MicroPrintf("RegisterOps failed with status: %d\n", status);
+    return;
+  }
+
+  static tflite::MicroInterpreter micro_interpreter(model, op_resolver, tensor_arena,
+                                           kTensorArenaSize);
+  interpreter = &micro_interpreter; // Set the interpreter global variable
+
+
+
+  // 4. Allocate tensors.
+  // This step is necessary to allocate memory for the input, output, and intermediate tensors.
+  // The AllocateTensors() function will allocate memory for the tensors based on the model's
+  // input and output shapes, and the tensor arena size.
+  // It will also set the input and output tensors in the interpreter.
+  status = interpreter->AllocateTensors();
+  if (status != kTfLiteOk) {
+    MicroPrintf("AllocateTensors failed with status: %d\n", status);
+    return;
+  }
+
+
+  // 5. Get input and output tensors.
+
+  input = interpreter->input(0);
+  if (input == nullptr) {
+    MicroPrintf("Input tensor is null\n");
+    return;
+  }
+
+  output = interpreter->output(0);
+  if (output == nullptr) {
+    MicroPrintf("Output tensor is null\n");
+    return;
+  }
+
+  output_scale = output->params.scale;
+  output_zero_point = output->params.zero_point;
+
 
 
 
@@ -71,41 +164,6 @@ void loop() {
   // 2. Run inference.
   // 3. Obtain the output from the model and do something with it.
 
-  //// SNIP 9 (set the input tensor with a value to feed into the model.)
-
-  //// SNIP 10 (Run inference.)
-
-  //// SNIP 11 (Obtain the output from the model and do something with it.)
-
-
-
-  // For our test case, we will increment the inference_count and reset it (this is not a general thing you'd do)
-  // Increment the inference_counter, and reset it if we have reached
-  // the total number per cycle
-  inference_count += 1;
-  if (inference_count >= kInferencesPerCycle) inference_count = 0;
- 
-} 
-
-
-// Arduino wouldn't have this main function, but we do for testing
-int main(int argc, char* argv[]) {
-
-    setup();
-    while(true){ 
-        loop();
-        sleep(1);
-    }
-
-}
-
-
-
-////////////////////////////////////////////////
-
-
-
-  //// BEGIN SNIP 9
 
   // 1. Set the input tensor with a value to feed into the model.   
 
@@ -127,11 +185,17 @@ int main(int argc, char* argv[]) {
   //input->data.int8[0] = x_quantized;
 
 
-  //// END SNIP 9
 
 
+  // 2. Run inference.   
 
-  //// BEGIN SNIP 11
+  // Run inference, and report any error
+  TfLiteStatus invoke_status = interpreter->Invoke();
+  if (invoke_status != kTfLiteOk) {
+    MicroPrintf("Invoke failed on x: %f\n", static_cast<double>(x));
+    return;
+  }
+
 
   //  3. Obtain the output from the model and do something with it.
 
@@ -147,173 +211,24 @@ int main(int argc, char* argv[]) {
   // Output the results. A custom HandleOutput function can be implemented
   // for each supported hardware target.
   HandleOutput(x, y);
-  //// END SNIP 11
 
 
+  // For our test case, we will increment the inference_count and reset it (this is not a general thing you'd do)
+  // Increment the inference_counter, and reset it if we have reached
+  // the total number per cycle
+  inference_count += 1;
+  if (inference_count >= kInferencesPerCycle) inference_count = 0;
+ 
+} 
 
 
-//// BEGIN SNIP 1
+// Arduino wouldn't have this main function, but we do for testing
+int main(int argc, char* argv[]) {
 
-#include <math.h>
-#include <unistd.h> // For sleep()
+    setup();
+    while(true){ 
+        loop();
+        sleep(1);
+    }
 
-// For Arduino, use:
-//#include <Chirale_TensorFlowLite.h>
-
-// could use int8 or float version, we'll use float
-//#include "hello_world_int8_model_data.h"
-#include "hello_world_float_model_data.h"
-
-#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
-#include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/micro/micro_log.h"
-#include "tensorflow/lite/micro/system_setup.h"
-#include "tensorflow/lite/schema/schema_generated.h"
-
-//// END SNIP 1
-
-
-  //// BEGIN SNIP 10
-
-  // 2. Run inference.   
-
-  // Run inference, and report any error
-  TfLiteStatus invoke_status = interpreter->Invoke();
-  if (invoke_status != kTfLiteOk) {
-    MicroPrintf("Invoke failed on x: %f\n", static_cast<double>(x));
-    return;
-  }
-
-  //// END SNIP 10
-
-
-
-//// BEGIN SNIP 2
-
-// some globals used 
-const tflite::Model* model = nullptr;  // the model
-tflite::MicroInterpreter* interpreter = nullptr; // the interpreter (what can perform inference)
-TfLiteTensor* input = nullptr; // the input tensor (where we put the input data)
-TfLiteTensor* output = nullptr; // the output tensor (where we get the output data)
-float output_scale = 0.0f; // for quantized models, the scale used to convert the output data
-int output_zero_point = 0; // for quantized models, the zero point used to convert the output data
-
-// the tensor_arena is the memory used for tensors (inputs, outputs, and intermediate tensors)
-  // Determining the Arena size is a bit of trial/error. 
-  // Docs say The exact arena usage can be determined  using the RecordingMicroInterpreter.
-constexpr int kTensorArenaSize = 2000;
-alignas(16) uint8_t tensor_arena[kTensorArenaSize];  // Keep aligned to 16 bytes for CMSIS
-
-//// END SNIP 2
-
-
-
-
-
-  //// BEGIN SNIP 5
-
-  // 2. Map the model into a usable data structure.
-
-  // using float:
-  model = tflite::GetModel(g_hello_world_float_model_data);
-  // if using int8
-  // model = tflite::GetModel(g_hello_world_int8_model_data);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
-    MicroPrintf(
-        "Model provided is schema version %d not equal "
-        "to supported version %d.",
-        model->version(), TFLITE_SCHEMA_VERSION);
-    return;
-  }
-
-  //// END SNIP 5
-
-
-  //// BEGIN SNIP 4 
-
-  // 1. Initialize the target system.
-
-  // The tflite::InitializeTarget function, invoked during startup, is actually a user-implemented function. 
-  // It serves as a call to the system_setup.cc file, which users are expected to implement as part of integrating 
-  // TensorFlow Lite for Microcontrollers. This function provides a location for the initialization of hardware 
-  // drivers, such as UART, SPI, etc.
-  // For Arduino, see:
-  // https://github.com/spaziochirale/Chirale_TensorFlowLite/blob/main/src/tensorflow/lite/micro/system_setup.cpp
-
-  tflite::InitializeTarget();
-
-  //// END SNIP 4
-
-
-
-  //// BEGIN SNIP 6
-
-  // 3. Create an operator resolver.
-
-  
-  static HelloWorldOpResolver op_resolver;  // static means the allocation of memory survives the function call
-  status = RegisterOps(op_resolver);
-  if (status != kTfLiteOk) {
-    MicroPrintf("RegisterOps failed with status: %d\n", status);
-    return;
-  }
-
-  static tflite::MicroInterpreter micro_interpreter(model, op_resolver, tensor_arena,
-                                           kTensorArenaSize);
-  interpreter = &micro_interpreter; // Set the interpreter global variable
-
-
-  //// END SNIP 6
-
-
-//// BEGIN SNIP 3
-
-// Define a custom operator resolver for the Hello World example.
-// This resolver only needs one operator, the FullyConnected operator 
-using HelloWorldOpResolver = tflite::MicroMutableOpResolver<1>;
-TfLiteStatus RegisterOps(HelloWorldOpResolver& op_resolver) {
-  TF_LITE_ENSURE_STATUS(op_resolver.AddFullyConnected());
-  return kTfLiteOk;
 }
-
-//// END SNIP 3
-
-
-  //// BEGIN SNIP 7
-
-  // 4. Allocate tensors.
-  // This step is necessary to allocate memory for the input, output, and intermediate tensors.
-  // The AllocateTensors() function will allocate memory for the tensors based on the model's
-  // input and output shapes, and the tensor arena size.
-  // It will also set the input and output tensors in the interpreter.
-  status = interpreter->AllocateTensors();
-  if (status != kTfLiteOk) {
-    MicroPrintf("AllocateTensors failed with status: %d\n", status);
-    return;
-  }
-
-
-  //// END SNIP 7
-
-
-  //// BEGIN SNIP 8
-
-  // 5. Get input and output tensors.
-
-  input = interpreter->input(0);
-  if (input == nullptr) {
-    MicroPrintf("Input tensor is null\n");
-    return;
-  }
-
-  output = interpreter->output(0);
-  if (output == nullptr) {
-    MicroPrintf("Output tensor is null\n");
-    return;
-  }
-
-  output_scale = output->params.scale;
-  output_zero_point = output->params.zero_point;
-
-
-  //// END SNIP 8
